c1c3f3325da495a303131639fd7d23d9704d6810
SINTA NURHALIFAH <92354983+sinta23nurhalif@users.noreply.github.com>
2022-06-13 20:42:09 +0700

    Update Stack Baju di Dalam Lemari.cpp

--- Stack Baju di Dalam Lemari.cpp
+++ Stack Baju di Dalam Lemari.cpp
@@ -2,10 +2,12 @@
 #include <stack>
 
 using namespace std;
-int main();
+
+int main(){
 int pilih;
 string input;
 
+do{
 cout <<"PROGRAM TUMPUKAN BAJU DI DALAM LEMARI" << endl;
 cout <<" 1. Masukan baju di lemari" << endl;
 cout <<" 2. Mengambil baju dari tumpukkan di lemari" << endl;
@@ -14,38 +16,43 @@ cout <<" 4. Melihat Tumpukan Baju di Lemari "<<endl;
 cout <<" 5. Hapus"<<endl;
 
 cout <<"Masukan Pilihan (1-5) : ";
-		cin >> pilih;
-		switch (pilih){		//percabangan switch case
-		case 1:
-			cout <<"Masukan Baju :"<<endl;
-			cin >> input;
-			tumpukan.push(input);
-			system("CLS");
-			cout <<endl;
-			break;
-		case 2:
-			if (tumpukan.empty()){		//kondisi jika data masih kosong
-				cout <<">>>>>Tumpukan Baju di Lemari Sudah Kosong<<<<<<" ;
-			}else{
-				cout <<"Tumpukan Baju ["<<tumpukan.top() <<"] telah di ambil" <<endl;
-				tumpukan.pop();
-			}
-			cout <<endl;
-			break;
-		case 3:
-			if(tumpukan.empty()){			//kondisi jika data masih kosong
-				cout <<">>>>>Tumpukan Baju dilemari Sudah Kosong<<<<<<" ;
-			}else{
-				while (!tumpukan.empty()){
-					cout <<"["<<tumpukan.top() <<"]"<<endl;
-					tumpukan.pop();
-					}
-				}
-			break;
-		case 4:
+cin >> pilih;
+	
+	switch (pilih){		//percabangan switch case
+	case 1:
+		cout <<"Masukan Baju :"<<endl;
+		cin >> input;
+		tumpukan.push(input);
+		system("CLS");
+		cout <<endl;
+		break;
+	case 2:
+		if (tumpukan.empty()){		//kondisi jika data masih kosong
+			cout <<">>>>>Tumpukan Baju di Lemari Sudah Kosong<<<<<<" ;
+		}else{
+			cout <<"Tumpukan Baju ["<<tumpukan.top() <<"] telah di ambil" <<endl;
+			tumpukan.pop();
+		}
+		cout <<endl;
+		break;
+	case 3:
+		if(tumpukan.empty()){			//kondisi jika data masih kosong
+			cout <<">>>>>Tumpukan Baju dilemari Sudah Kosong<<<<<<" ;
+		}else{
+			while (!tumpukan.empty()){
+			cout <<"["<<tumpukan.top() <<"]"<<endl;
+			tumpukan.pop();
+		}
+		}
+		break;
+	case 4:
 			cout <<"Jumlah Tumpukan Baju di Lemari ada  " <<tumpukan.size() <<" baju"<<endl;
-			break;
-		case 5:
+		break;
+	case 5:
 			system("CLS");
 		default:
 			cout <<"Pilihan tidak ada!";
+		}
+}while (pilih >=1 && pilih <=4);
+	   return 0;
+}
