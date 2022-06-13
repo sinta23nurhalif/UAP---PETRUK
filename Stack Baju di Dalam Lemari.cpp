e4a0400a0cf00d34cd3bb95bb969f2a25927dfd5
SINTA NURHALIFAH <92354983+sinta23nurhalif@users.noreply.github.com>
2022-06-13 19:29:28 +0700

    Update Stack Baju di Dalam Lemari.cpp

--- Stack Baju di Dalam Lemari.cpp
+++ Stack Baju di Dalam Lemari.cpp
@@ -12,3 +12,15 @@ cout <<" 2. Mengambil baju dari tumpukkan di lemari" << endl;
 cout<<" 3. Melihat Tumpukan Baju di Lemari" <<endl;
 cout <<" 4. Melihat Tumpukan Baju di Lemari "<<endl;
 cout <<" 5. Hapus"<<endl;
+
+cout <<"Masukan Pilihan (1-5) : ";
+		cin >> pilih;
+		
+			switch (pilih){			//percabangan switch case
+			case 1:
+					cout <<"Masukan Baju :"<<endl;
+					cin >> input;
+					tumpukan.push(input);
+				system("CLS");
+				cout <<endl;
+				break;
