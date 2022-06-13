155bf371a1f660aa8599b2e72c207fc758608738
Naufal Elliando Fatahillah <92315859+NaufalElliandoFatahillah@users.noreply.github.com>
2022-06-13 19:36:18 +0700

    Create Stack Baju di Dalam Lemari.cpp

--- Stack Baju di Dalam Lemari.cpp
+++ Stack Baju di Dalam Lemari.cpp
@@ -9,7 +9,7 @@ string input;
 cout <<"PROGRAM TUMPUKAN BAJU DI DALAM LEMARI" << endl;
 cout <<" 1. Masukan baju di lemari" << endl;
 cout <<" 2. Mengambil baju dari tumpukkan di lemari" << endl;
-cout <<" 3. Melihat Tumpukan Baju di Lemari" <<endl;
+cout<<" 3. Melihat Tumpukan Baju di Lemari" <<endl;
 cout <<" 4. Melihat Tumpukan Baju di Lemari "<<endl;
 cout <<" 5. Hapus"<<endl;
 
@@ -25,10 +25,20 @@ cout <<"Masukan Pilihan (1-5) : ";
 			break;
 		case 2:
 			if (tumpukan.empty()){		//kondisi jika data masih kosong
-			cout <<">>>>>Tumpukan Baju di Lemari Sudah Kosong<<<<<<" ;
+				cout <<">>>>>Tumpukan Baju di Lemari Sudah Kosong<<<<<<" ;
 			}else{
-			cout <<"Tumpukan Baju ["<<tumpukan.top() <<"] telah di ambil" <<endl;
-			tumpukan.pop();
+				cout <<"Tumpukan Baju ["<<tumpukan.top() <<"] telah di ambil" <<endl;
+				tumpukan.pop();
 			}
 			cout <<endl;
+			break;
+		case 3:
+			if(tumpukan.empty()){			//kondisi jika data masih kosong
+				cout <<">>>>>Tumpukan Baju dilemari Sudah Kosong<<<<<<" ;
+			}else{
+				while (!tumpukan.empty()){
+					cout <<"["<<tumpukan.top() <<"]"<<endl;
+					tumpukan.pop();
+					}
+				}
 			break;
