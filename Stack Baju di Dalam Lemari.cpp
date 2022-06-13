70cd88835f3d6da19c66696a674ffae56c684c48
Nurul Fadilah <92356807+Nurulfadilah21@users.noreply.github.com>
2022-06-13 19:28:40 +0700

    Update Stack Baju di Dalam Lemari.cpp

--- Stack Baju di Dalam Lemari.cpp
+++ Stack Baju di Dalam Lemari.cpp
@@ -10,3 +10,5 @@ cout <<"PROGRAM TUMPUKAN BAJU DI DALAM LEMARI" << endl;
 cout <<" 1. Masukan baju di lemari" << endl;
 cout <<" 2. Mengambil baju dari tumpukkan di lemari" << endl;
 cout<<" 3. Melihat Tumpukan Baju di Lemari" <<endl;
+cout <<" 4. Melihat Tumpukan Baju di Lemari "<<endl;
+cout <<" 5. Hapus"<<endl;
