056d0be1a5e77afb484fb59a7ec9c73a8047ee89
Naufal Elliando Fatahillah <92315859+NaufalElliandoFatahillah@users.noreply.github.com>
2022-06-15 20:54:43 +0700

    Update Stack Baju di Dalam Lemari.cpp

--- Stack Baju di Dalam Lemari.cpp
+++ Stack Baju di Dalam Lemari.cpp
@@ -1,63 +1,71 @@
 #include <iostream>
 #include <stack>	//stl
+
 using namespace std;
 
 int main(){
-stack <string> tumpukan;	//deklarasi variabel
-int pilih;
-string input;
+	stack <string> tumpukan;	//deklarasi variabel
+	int pilih;
+	string input;
 
 do{		//perulangan do while
-cout <<"=================================================" <<endl; 
-cout <<"======PROGRAM TUMPUKAN BAJU DI DALAM LEMARI======" << endl;
-cout <<" 1. Masukan baju di lemari" << endl;
-cout <<" 2. Mengambil baju dari tumpukkan di lemari" << endl;
-cout <<" 3. Melihat tumpukan baju di lemari" <<endl;
-cout <<" 4. Hapus "<<endl;
-cout <<" 5. Melihat jumlah tumpukan baju di lemari" <<endl;
-cout <<" 6. keluar Program" <<endl;
-cout <<"=================================================" <<endl;
+cout <<"\t\t\t\t=================================================" <<endl; 
+cout <<"\t\t\t\t======PROGRAM TUMPUKAN BAJU DI DALAM LEMARI======" << endl;
+cout <<"\t\t\t\t 1. Masukan baju di lemari" << endl;
+cout <<"\t\t\t\t 2. Mengambil baju dari tumpukkan di lemari" << endl;
+cout <<"\t\t\t\t 3. Melihat tumpukan baju di lemari" <<endl;
+cout <<"\t\t\t\t 4. Hapus "<<endl;
+cout <<"\t\t\t\t 5. Melihat jumlah tumpukan baju di lemari" <<endl;
+cout <<"\t\t\t\t 6. keluar Program" <<endl;
+cout <<"\t\t\t\t=================================================" <<endl;
 	
-cout <<"Masukan Pilihan (1-6) : ";
+cout <<"\t\t\t\t Masukan Pilihan (1-6) : ";
 cin >> pilih;
 	
 	switch (pilih){		//percabangan switch case
 	case 1:
-		cout<<"============================================"<<endl;
-		cout<<" Masukan Baju  :" ;
+		cout <<"\t\t\t\t============================================"<<endl;
+		cout <<"\t\t\t\tMasukan Baju  :" ;
 		cin >> input;		//Memasukan Baju dilemari
 		tumpukan.push(input);
 		system("CLS");
+		cout <<endl;
+		cout <<endl;
 		cout <<endl;
 		break;
 			
 	case 2:
 		if (tumpukan.empty()){		//kondisi jika data masih kosong
-			cout <<"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"<<endl ;
-			cout <<">>>>>Tumpukan Baju di Lemari Sudah Kosong gan<<<<<<"<<endl ;
-			cout <<"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"<<endl ;
+			cout <<"\t\t\t\t~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"<<endl ;
+			cout <<"\t\t\t\t>>>>>Tumpukan Baju di Lemari Sudah Kosong gan<<<<<<"<<endl ;
+			cout <<"\t\t\t\t~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"<<endl ;
 		}else{
-			cout<<"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"<<endl;
-			cout <<"Tumpukan Baju ["<<tumpukan.top() <<"] telah di ambil" <<endl;
+			cout <<"\t\t\t\t~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"<<endl;
+			cout <<"\t\t\t\tTumpukan Baju ["<<tumpukan.top() <<"] telah di ambil" <<endl;
 			tumpukan.pop();
-			cout<<"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"<<endl;
+			cout <<"\t\t\t\t~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"<<endl;
 		}
+		cout <<endl;
+		cout <<endl;
 		cout <<endl;
 		break;
 			
 	case 3:
 		if(tumpukan.empty()){			//kondisi jika data masih kosong
-			cout <<"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"<<endl;
-			cout <<">>>>>Tumpukan Baju dilemari Sudah Kosong gan<<<<<<"<<endl;
-			cout <<"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"<<endl;
+			cout <<"\t\t\t\t~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"<<endl;
+			cout <<"\t\t\t\t>>>>>Tumpukan Baju dilemari Sudah Kosong gan<<<<<<"<<endl;
+			cout <<"\t\t\t\t~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"<<endl;
 		}else{
-			cout <<"Tumpukan baju di dalam lemari : " <<endl;
+			cout <<"\t\t\t\tTumpukan baju di dalam lemari : " <<endl;
 			while (!tumpukan.empty()){
-			cout <<"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"<<endl;
-			cout <<"["<<tumpukan.top() <<"]"<<endl;		//menampilkan tumpukan baju
+			cout <<"\t\t\t\t~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"<<endl;
+			cout <<"\t\t\t\t ["<<tumpukan.top() <<"]"<<endl;		//menampilkan tumpukan baju
 			tumpukan.pop();
+			}
 		}
-		}
+		cout <<endl;
+		cout <<endl;
+		cout <<endl;
 		break;
 			
 	case 4:
@@ -65,24 +73,26 @@ cin >> pilih;
 		break;
 			
 	case 5:
-		cout<<"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"<<endl;
-		cout <<"Jumlah Tumpukan Baju di Lemari ada  " <<tumpukan.size() <<" baju gan"<<endl; 	//melihat jumlah tumpukan baju
-		cout<<"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"<<endl;
+		cout <<"\t\t\t\t~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"<<endl;
+		cout <<"\t\t\t\t Jumlah Tumpukan Baju di Lemari ada  " <<tumpukan.size() <<" baju gan"<<endl; 	//melihat jumlah tumpukan baju
+		cout <<"\t\t\t\t~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"<<endl;
 		break;
 			
 	case 6:
-		cout<<"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"<<endl;
-		cout <<"Program Selesai"<<endl;
-		cout<<"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"<<endl;
+		cout <<"\t\t\t\t~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"<<endl;
+		cout <<"\t\t\t\t Program Selesai"<<endl;
+		cout <<"\t\t\t\t~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"<<endl;
 		exit (0);
 		break;
 			
 	default:
-		cout<<"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"<<endl;
-		cout <<"Pilihan tidak ada gan, baca lagi coba pilihannya gan"<<endl;
-		cout<<"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"<<endl;
+		cout <<"\t\t\t\t~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"<<endl;
+		cout <<"\t\t\t\t Pilihan tidak ada gan, baca lagi coba pilihannya gan"<<endl;
+		cout <<"\t\t\t\t~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"<<endl;
+		cout <<endl;
+		cout <<endl;
+		cout <<endl;
 		}
-	
-}while (pilih >=1 && pilih <=5);
-	   return 0;
+	}while (pilih >=1 && pilih <=5);
+	 return 0;
 }
